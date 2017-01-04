a870f19247b426d4d812e955c4c0aae920e04fec
Jona <mc1@hotmail.de>
2017-01-04 23:54:14 +0100

    Changes

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $website_charset     = "UTF-8";
+    $website_description = "Here your website description";
+    $website_keywords    = "Here, your, website, keywords";
+    $website_title       = "Here your website title";
+
+    $website_copyright        = "Here your website name";
+?>
